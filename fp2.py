bcaa204864597307cc5bcda1b21544671b971f58
brianjeon0722 <brianjeon@college.harvard.edu>
2026-04-17 03:26:08 +0000

    Thu, Apr 16, 2026, 11:26 PM -04:00

--- fp2.py
+++ fp2.py
@@ -34,15 +34,19 @@ def correct(full_name, guesses):
     print(f'Correct! This is the {full_name}.\n')
     return True, guesses
 
+def correct(full_name, guesses):
+    print(f'Correct! This is the {full_name}.\n')
+    return True, guesses  # already does this, good
+
 def check_guess(computer_opening, guesses):
     initial_guess = get_guess('What opening is this? ')
     opening_name = computer_opening.get('name')
     line_name = computer_opening.get('line_name')
 
-    if line_name == None: # if no lines
+    if line_name is None:
         full_name = computer_opening["name"]
         if opening_name in initial_guess:
-            correct(full_name, guesses)
+            return correct(full_name, guesses)  # ← return the result
         else:
             guesses += 1
             print(f'Not quite. You have {3 - guesses} more attempt(s).\n')
@@ -50,17 +54,16 @@ def check_guess(computer_opening, guesses):
                 print(f'This is the {full_name}.\n')
             return False, guesses
 
-    else: # if lines exist
+    else:
         full_name = f'{computer_opening["name"]} {line_name}'
-        if opening_name in initial_guess or line_name in initial_guess:
-            if line_name in initial_guess:
-                correct(full_name, guesses)
-            elif opening_name in initial_guess:
-                line_guess = get_guess(f'What line of the {opening_name} is this? ')
-                if line_name in line_guess:
-                    correct(full_name, guesses)
-                else:
-                    guesses += 1
+        if line_name in initial_guess:
+            return correct(full_name, guesses)  # ← return the result
+        elif opening_name in initial_guess:
+            line_guess = get_guess(f'What line of the {opening_name} is this? ')
+            if line_name in line_guess:
+                return correct(full_name, guesses)  # ← return the result
+            else:
+                guesses += 1
                 print(f'Not quite. You have {3 - guesses} more attempt(s).\n')
                 if guesses >= 3:
                     print(f'This is the {full_name}.\n')
@@ -73,22 +76,24 @@ def check_guess(computer_opening, guesses):
             return False, guesses
 
 
-
 def play_game(games):
     times_played = 0
 
     while times_played < games:
         guesses = 0
+        solved = False
         computer_opening = random.choice(openings)
 
-        print(f'### Game {times_played} ###')
+        print(f'### Game {times_played + 1} ###')  # ← +1 so it starts at 1
         board = chess.Board()
         for move in computer_opening['moves']:
             board.push_san(move)
             print(board)
             print('\n')
 
-        check_guess(computer_opening, guesses)
+        while not solved and guesses < 3:  # ← keep asking until solved or out of guesses
+            solved, guesses = check_guess(computer_opening, guesses)
+
         times_played += 1
 
 def main():
