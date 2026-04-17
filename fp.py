f2d91ed20631dd331747502ef61e9236ea2bde45
brianjeon0722 <brianjeon@college.harvard.edu>
2026-04-17 02:02:02 +0000

    Thu, Apr 16, 2026, 10:02 PM -04:00

--- fp.py
+++ fp.py
@@ -1,50 +1,93 @@
 import chess
 import random
 
-board = chess.Board()
-
 openings = [{
     'name': 'Sicilian',
     'line_name': 'Smith-Morra',
     'moves': ['e4', 'c5', 'd4', 'cxd4', 'c3']},
     {
     'name': 'Sicilian',
-    'line_name': 'Nimzowitsch',
-    'moves': ['e4', 'c5', 'Nf3', 'Nf6']},
+    'line_name': 'Accelerated Dragon',
+    'moves': ['e4', 'c5', 'Nf3', 'Nc6' 'd4', 'cxd4', 'Nxd4', 'g6']},
+    {
+    'name': 'Sicilian',
+    'line_name': 'Dragon',
+    'moves': ['e4', 'c5', 'Nf3', 'd6', 'd4', 'cxd4', 'Nxd4', 'Nf6', 'Nc3', 'g6']},
     {
     'name': 'Sicilian',
     'line_name': 'Grand Prix',
-    'moves': ['e4', 'c5', 'Nc3', 'Nc6', 'f4']},
+    'moves': ['e4', 'c5', 'Nc3', 'Nc6' 'f4']},
+    {
+    'name': 'Ruy Lopez',
+    'moves': ['e4', 'e5', 'Nf3', 'Nc6', 'Bb5']},
     {
     'name': 'Scotch',
-    'line_name': 'Traditional',
     'moves': ['e4', 'e5', 'Nf3', 'Nc6', 'd4']
     }
 ]
 
-computer_opening = random.choice(openings)
+games = int(input('How many times would you like to play? ').strip())
 
-for i in computer_opening['moves']:
-    board.push_san(i)
-    print(board)
-    print('\n')
-guesses = 0
-while guesses < 3:
-    initial_guess = str(input('What opening is this? ').lower().strip())
-    if computer_opening['line_name'].lower().strip() == initial_guess:
-        print(f'Correct! This is the {computer_opening['name']} {computer_opening['line_name']}.')
-        break
-    elif computer_opening['name'].lower().strip() == initial_guess:
-        print(f'Correct! This is the {computer_opening['name']}.')
-        line_guess = str(input(f'What line of the {computer_opening['name']} is this? ').lower().strip())
-        if computer_opening['line_name'].lower().strip() == line_guess:
-            print(f'Correct! This is the {computer_opening['name']} {computer_opening['line_name']}')
-            break
+times_played = 0
+wins = 0
+
+while times_played < games:
+
+    times_played += 1
+
+    computer_opening = random.choice(openings)
+
+    print(f'### Game {times_played} ###')
+    board = chess.Board() # reset chess board
+
+    for i in computer_opening['moves']:
+        board.push_san(i)
+        print(board)
+        print('\n')
+
+    guesses = 0
+
+    while guesses < 3:
+        initial_guess = str(input('What opening is this? ')).lower().strip()
+
+        if computer_opening.get('line_name') is not None:
+
+            if computer_opening['line_name'].lower().strip() == initial_guess:
+                print(f'Correct! This is the {computer_opening["name"]} {computer_opening["line_name"]}.\n')
+                wins += 1
+                break
+
+            elif computer_opening['name'].lower().strip() == initial_guess:
+                line_guess = str(input(f'What line of the {computer_opening["name"]} is this? ')).lower().strip()
+
+                if computer_opening['line_name'].lower().strip() == line_guess:
+                    print(f'Correct! This is the {computer_opening["name"]} {computer_opening["line_name"]}.\n')
+                    wins += 1
+                    break
+
+                else:
+                    guesses += 1
+                    print(f'Not quite. You have {3 - guesses} more attempt(s).\n')
+                    if guesses >= 3:
+                        print(f'This is the {computer_opening["name"]} {computer_opening["line_name"]}.\n')
+
+            else:
+                guesses += 1
+                print(f'Not quite. You have {3 - guesses} more attempt(s).\n')
+                if guesses >= 3:
+                    print(f'This is the {computer_opening["name"]} {computer_opening["line_name"]}.\n')
+
         else:
-            guesses += 1
-            print(f'Not quite. You have {3 - guesses} more attempt(s).')
-    else:
-         guesses += 1
-         print(f'Not quite. You have {3 - guesses} more attempt(s).')
+            if computer_opening['name'].lower().strip() == initial_guess:
+                print(f'Correct! This is the {computer_opening["name"]}.\n')
+                wins += 1
+                break
+            else:
+                guesses += 1
+                print(f'Not quite. You have {3 - guesses} more attempt(s).\n')
+                if guesses >= 3:
+                    print(f'This is the {computer_opening["name"]}.\n')
 
+
+print(f'You got {wins}/{games} correct!')
 
---
+++ test.py
@@ -0,0 +1,34 @@
+from Openix import ChessOpeningsLibrary
+import re
+
+library = ChessOpeningsLibrary()
+loaded_count = library.load_builtin_openings()
+
+all_openings = library.get_all_openings()
+
+print(f"Total openings: {len(all_openings)}")
+
+# for opening in all_openings[500:530]:
+#     print(opening.name)
+#     print(opening.eco_code)
+
+all_sicilian_names = {}
+
+for a in range(20, 100):
+    for opening in library.find_by_eco(f'B{a}'):
+        opening_name = opening.name
+        if opening.name[0:opening.name.find(':')] == 'Sicilian Defense':
+            opening_name = opening_name.replace("Sicilian Defense", "Sicilian")
+        if opening.name[0:opening.name.find(':')] == 'Sicilian':
+            if re.search(r"[1234567890]", opening_name) != None:
+                opening_name = opening_name[:re.search(r"[1234567890]", opening_name).start()-1]
+            if re.search(r"[,]", opening_name) != None:
+                opening_name = opening_name[:re.search(r"[,]", opening_name).start()]
+            all_sicilian_names[opening_name] = opening.moves_str
+
+for name in sorted(all_sicilian_names):
+    print(name)
+    # print(all_sicilian_names[name])
+
+
+
